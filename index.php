e4378f515dba588a148c576f87bc9b12d0e3499a
Carlos Vallory <carlvallory@gmail.com>
2021-01-28 10:13:13 -0300

    param boolean delete

--- index.php
+++ index.php
@@ -8,24 +8,28 @@
 
 $csvPath = "pruebas/test.csv";
 $filePath = [];
+$delete = false; //$delete = [];
 
 $handle = fopen($csvPath, "r");
 while (($data = fgetcsv($handle)) !== FALSE) {
     $filePath[] = $data[0];
+    //$delete[] = $data[1];
 }
 
-foreach($filePath as $fpath) {
+foreach($filePath as $key => $fpath) {
     if(checkPathPermission($fpath)){
-        findNReplace($fpath);
+        findNReplace($fpath, $delete);
     }
 }
 
-function findNReplace($file, $remove = false) {
+function findNReplace($file, $remove) {
     $wStart = "/*aeR4Choc_start*/";
     $wEnd   = "/*aeR4Choc_end*/";
 
     $original = $file;
     $temp = "ToBeRemoved";
+
+    echo "Starting with File -> " . $file . "<br>";
 
     $fileInfo       = pathinfo($original);
     $oriFilename    = $fileInfo['filename'];
@@ -43,9 +47,9 @@ function findNReplace($file, $remove = false) {
     if($moveFile) {
         $newOriginalFile = fopen($original, "w") or die("Unable to create a file!"); 
         if($newOriginalFile){
-            echo "Creating: Done -> " . $file . "<br>";
+            echo "Creating: Done -> <br>";
         } else {
-            echo "Creating: Fail -> " . $file . "<br>";
+            echo "Creating: Fail -> <br>";
         }
 
         $handle = fopen($oldOriginal, "r");
@@ -75,17 +79,17 @@ function findNReplace($file, $remove = false) {
             $fcHandle = fclose($handle);
             $fcNOF = fclose($newOriginalFile);
             if($fcHandle){
-                echo "Reading: Done -> " . $file . "<br>";
+                echo "Reading: Done -> <br>";
             } else {
-                echo "Reading: Fail -> " . $file . "<br>";
+                echo "Reading: Fail -> <br>";
             }
             if($fcNOF){
-                echo "Writing: Done -> " . $file . "<br>";
+                echo "Writing: Done -> <br>";
             } else {
-                echo "Writing: Fail -> " . $file . "<br>";
+                echo "Writing: Fail -> <br>";
             }
         } else {
-            echo "Reading: Fail -> " . $file . "<br>";
+            echo "Reading: Fail -> <br>";
             return false;
         }
     }
@@ -96,17 +100,19 @@ function findNReplace($file, $remove = false) {
 
     if($remove == true){
         if(file_exists($oldOriginal) == false) {
-            echo "Removing: Done -> " . $file . "<br>";
+            echo "Removing: Done -> <br>";
         } else {
-            echo "Removing: Fail -> " . $file . "<br>";
+            echo "Removing: Fail -> <br>";
         }
     } else {
         if(file_exists($oldOriginal) == true) {
-            echo "Created: Done -> " . $file . "<br>";
+            echo "Created: Done -> <br>";
         } else {
-            echo "Created: Fail -> " . $file . "<br>";
+            echo "Created: Fail -> <br>";
         }
     }
+
+    echo "Ending with File " . $file . "<br>";
 
     return true;
 }
