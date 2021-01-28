a4acfb5fc5986a42d22659cc65753c36dfbc4ce7
Carlos Vallory <carlvallory@gmail.com>
2021-01-27 22:10:15 -0300

    itera un csv

--- index.php
+++ index.php
@@ -1,52 +1,73 @@
 <?php 
 
-$wStart = "/*aeR4Choc_start*/";
-$wEnd   = "/*aeR4Choc_end*/";
+/**
+ * Recorrer CSV 
+ * El CSV debe contener un listado de archivos 
+ * a ser recorridos por el script
+ */
 
-$original = "pruebas/index.php";
-$temp = "ToBeRemoved";
+$csvPath = "pruebas/test.csv";
+$filePath = [];
 
-$fileInfo       = pathinfo($original);
-$oriFilename    = $fileInfo['filename'];
-$oriExtension   = $fileInfo['extension'];
-$oriDir         = $fileInfo['dirname'];
-$oldOriginal    = $oriDir . DIRECTORY_SEPARATOR . $oriFilename . "." . $temp . "." . $oriExtension;
+$handle = fopen($csvPath, "r");
+while (($data = fgetcsv($handle)) !== FALSE) {
+    $filePath[] = $data[0];
+}
 
-$moveFile = rename($original, $oldOriginal);
+foreach($filePath as $fpath) {
+    findNReplace($fpath);
+}
+
+function findNReplace($file) {
+    $wStart = "/*aeR4Choc_start*/";
+    $wEnd   = "/*aeR4Choc_end*/";
+
+    $original = $file;
+    $temp = "ToBeRemoved";
+
+    $fileInfo       = pathinfo($original);
+    $oriFilename    = $fileInfo['filename'];
+    $oriExtension   = $fileInfo['extension'];
+    $oriDir         = $fileInfo['dirname'];
+    $oldOriginal    = $oriDir . DIRECTORY_SEPARATOR . $oriFilename . "." . $temp . "." . $oriExtension;
+
+    $moveFile = rename($original, $oldOriginal);
 
 
-if($moveFile) {
-    $newOriginalFile = fopen($original, "w") or die("Unable to create a file!"); 
-    
+    if($moveFile) {
+        $newOriginalFile = fopen($original, "w") or die("Unable to create a file!"); 
+        
 
-    $handle = fopen($oldOriginal, "r");
-    if ($handle) {
-        while (($line = fgets($handle)) !== false) {
-            
-            if(strpos($line, $wStart) === false) {
-                fwrite($newOriginalFile, $line);
-            } else {
-                /**
-                 * Buscar coincidencias y retornar las posiciones de inicio y final
-                 * Replazar el contenido
-                 * y escribir en el nuevo archivo.
-                 */
-                $sSPos = strpos($line, $wStart); 
-                $sEPos = strpos($line, $wEnd); 
+        $handle = fopen($oldOriginal, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                
+                if(strpos($line, $wStart) === false) {
+                    fwrite($newOriginalFile, $line);
+                } else {
+                    /**
+                     * Buscar coincidencias y retornar las posiciones de inicio y final
+                     * Replazar el contenido
+                     * y escribir en el nuevo archivo.
+                     */
+                    $sSPos = strpos($line, $wStart); 
+                    $sEPos = strpos($line, $wEnd); 
 
-                $wEndCount = strlen($wEnd);
-                $sELen = $sEPos-($sSPos-$wEndCount);
+                    $wEndCount = strlen($wEnd);
+                    $sELen = $sEPos-($sSPos-$wEndCount);
 
-                $newLine = substr_replace($line, '', $sSPos, $sELen);
+                    $newLine = substr_replace($line, '', $sSPos, $sELen);
 
-                fwrite($newOriginalFile, $newLine);
+                    fwrite($newOriginalFile, $newLine);
+                }
             }
-        }
 
-        fclose($handle);
-    } else {
-        // error opening the file.
+            fclose($handle);
+        } else {
+            return false;
+        }
     }
+    return true;
 }
 
 ?>
